8e58c0d35ec730d095bfe928412da7b2499acbf1
Maxym Dyachenko <max.dyachenko@inbox.ru>
2017-11-22 13:28:19 +0200

    add joomla and helloworld component

---
+++ administrator/components/com_artgallery/sql/install.mysql.utf8.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS `#__gallerys_list`;
+DROP TABLE IF EXISTS `#__users_imgs`;
+CREATE TABLE `#__gallerys_list` (
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+	`greeting` VARCHAR(25) NOT NULL,
+	`published` tinyint(4) NOT NULL DEFAULT '1',
+	PRIMARY KEY (`id`)
+)
+	ENGINE =MyISAM
+	AUTO_INCREMENT =0
+	DEFAULT CHARSET =utf8;
+
+CREATE TABLE `#__users_imgs` (
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+	`greeting` VARCHAR(25) NOT NULL,
+	`published` tinyint(4) NOT NULL DEFAULT '1',
+	PRIMARY KEY (`id`)
+)
+	ENGINE =MyISAM
+	AUTO_INCREMENT =0
+	DEFAULT CHARSET =utf8;
+
---
+++ administrator/components/com_artgallery/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS `#__gallerys_list`;
+DROP TABLE IF EXISTS `#__users_imgs`;
